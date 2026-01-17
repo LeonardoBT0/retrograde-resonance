a1b8d9cb7bb548c4bc7b9a593777c3efe250b490
Leonardo <leobarrera445@gmail.com>
2026-01-17 14:13:11 -0600

    Actualización3

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,8 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString: import.meta.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-  max: 1
+  connectionString: import.meta.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
